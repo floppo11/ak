97b942e636ccf7292b1b4ad3c0d51ffafb61bf0b
Floris Riel <Floris.riel@gmail.com>
2020-10-20 16:57:43 +0200

    update

--- index.php
+++ index.php
@@ -3,25 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Productief procrastinatie</title>
+    <title>AK site</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <form method="POST" action="index.php">
-        <p>Maak een gebruiker</p>
+        <h1>Zoek je AK vraag op</h1>
         <input type="text" class="user" name="naam" placeholder="gebruikersnaam">
         <input type="submit" value="KLIK">
     </form>
     <?php
         
-        $db = "INLOGGEN";
+        $db = "AARDRIJKSKUNDE";
         $ip = "localhost";
         $pw = "";
         $user = "root";
         $conn = new mysqli($ip, $user, $pw, $db);
 
-        if (isset($_POST['naam'])) {
-            $naam = $_POST["naam"];
+        if (isset($_POST['info])) {
+            $naam = $_POST["info"];
             $query = "SELECT * FROM products WHERE naam LIKE '%$naam%'";
             $result = $conn->query($query);
 
---
+++ queries.sql
@@ -0,0 +1,15 @@
+CREATE TABLE users (
+    naam TINYTEXT,
+    achternaam TINYTEXT,
+    leefijd TINYINT
+);
+
+INSERT INTO users VALUES(
+    "Artsiom",
+    "Dzenisiuk",
+    12
+);
+
+CREATE TABLE products (
+    naam TINYTEXT
+);
