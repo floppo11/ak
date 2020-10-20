ec1051f32089b0922d207ff19626bb2d2abe6e53
Floris Riel <Floris.riel@gmail.com>
2020-10-20 16:41:37 +0200

    toevoegen eerste bestanden

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Productief procrastinatie</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <form method="POST" action="index.php">
+        <p>Maak een gebruiker</p>
+        <input type="text" class="user" name="naam" placeholder="gebruikersnaam">
+        <input type="submit" value="KLIK">
+    </form>
+    <?php
+        
+        $db = "INLOGGEN";
+        $ip = "localhost";
+        $pw = "";
+        $user = "root";
+        $conn = new mysqli($ip, $user, $pw, $db);
+
+        if (isset($_POST['naam'])) {
+            $naam = $_POST["naam"];
+            $query = "SELECT * FROM products WHERE naam LIKE '%$naam%'";
+            $result = $conn->query($query);
+
+            echo "Hier zijn de resultaten";
+            echo "<table>";
+            for ($i = 0; $i < $result->num_rows; $i++) {
+                $row = $result->fetch_assoc();
+                echo "<tr><td>{$row['naam']}</td></tr>";
+            }
+
+            echo "</table";
+// namen van de tabel rijen
+//| naam    | achternaam | leefijd
+        }
+    ?>
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+</body>
+</html>
